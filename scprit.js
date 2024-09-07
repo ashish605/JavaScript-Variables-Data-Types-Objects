36242f7f87faba9696a58b2b14429f29fa0c0ae2
ashish605 <ashishvs605@gmail.com>
2024-09-07 15:44:38 +0530

    Initial commit

---
+++ scprit.js
@@ -0,0 +1,24 @@
+var a=6;
+var b=7;
+console.log("anwser is " + a+b)
+
+var c="harry";
+console.log(typeof a,typeof b,typeof c)
+
+let d=66;
+{
+    let d=77;
+    console.log(d)
+}
+console.log(d)
+
+
+let x="harry"; 
+let y=2.33;
+let z=22;
+const p=true;
+let q=undefined;
+let r=null;
+
+console.log(x,y,z,p,q,r);
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r);
---
+++ scprit1.js
@@ -0,0 +1,19 @@
+var a=1;
+let b=2;
+console.log("anwser " + a + b)
+
+console.log(typeof 2)
+
+
+
+let o={
+    "name":"asish",
+    "role":"full stack devloper"
+
+}
+
+console.log(o)
+
+o.salary="5000000"
+console.log(o)
+
